131d6df06db3f055fd1304abcb9e32ec192a8e18
CamreshJames <90922122+CamreshJames@users.noreply.github.com>
2023-06-20 11:26:46 +0300

    new chenge

--- pos/settings.py
+++ pos/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PosDB',                      
+        'USER': 'postgres',
+        'PASSWORD': '5432',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
