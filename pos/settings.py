a3886d6e5614dac09f7c2ba56780c98030d18e4e
Manaseh1 <manasehnjoroge7@gmail.com>
2023-06-21 19:13:29 +0300

    Signup and Login

--- pos/settings.py
+++ pos/settings.py
@@ -37,8 +37,10 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 APPS = [
     'accounts',
 ]
