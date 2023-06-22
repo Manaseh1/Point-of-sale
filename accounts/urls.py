fe5a419b27c4af076476770823f840e7938be104
Manaseh1 <manasehnjoroge7@gmail.com>
2023-06-22 12:01:21 +0300

    changing changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,5 @@ appname = 'accounts'
 
 urlpatterns = [
     path('',home,name='home'),
-<<<<<<< HEAD
-    path('login/',signin,name='login'),
-    path('password-change/')
-=======
     path('Login/',signin,name='login'),
->>>>>>> 1ea3eec86982ddd4258e2c17a564dcb525e2908e
 ]
