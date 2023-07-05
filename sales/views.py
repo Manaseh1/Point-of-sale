bf2c6d5e33afcfe4dfbdc875d8f3c42c14309c4d
CamreshJames <90922122+CamreshJames@users.noreply.github.com>
2023-07-05 08:50:06 +0300

    listing the salesitems

--- sales/views.py
+++ sales/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .models import Sales, salesItems
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 @login_required
 def sales_dashboard(request):
     return render(request, 'sales_dashboard.html')
 @login_required
 def pos(request):
-    return render(request, 'pos.html')
+    products = Product.objects.all()
+    sales = Sales.objects.all()
+    salesitems = salesItems.objects.all()
+    context = {
+        'products': products,
+        'sales': sales,
+        'salesitems': salesitems
+    }
+    return render(request, 'pos.html', context)
