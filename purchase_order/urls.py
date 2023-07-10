d550d55d86d751e128672c856594504d2338dd2f
Manaseh1 <manasehnjoroge7@gmail.com>
2023-07-10 14:45:38 +0300

    product purchase order

--- purchase_order/urls.py
+++ purchase_order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import purchase_order
+from .views import *
 
 app_name = 'purchase_order'
 urlpatterns = [
     path('',purchase_order,name='purchase_order'),
+    path('save-form-data/', save_form_data, name='save_form_data'),
+
 ]
